5a0166df9424778708759dd682b1b7d672e8246b
Cesar Cachay <cesarcachay1996@gmail.com>
2026-08-25 15:58:04 -0500

    feat: add pokemon starter foundation

---
+++ packages/shared/src/pokemon/pokemon-starter.ts
@@ -0,0 +1,61 @@
+export const POKEMON_STARTERS = {
+  BULBASAUR: {
+    speciesId: 1,
+    level: 5,
+  },
+
+  CHARMANDER: {
+    speciesId: 4,
+    level: 5,
+  },
+
+  SQUIRTLE: {
+    speciesId: 7,
+    level: 5,
+  },
+
+  CHIKORITA: {
+    speciesId: 152,
+    level: 5,
+  },
+
+  CYNDAQUIL: {
+    speciesId: 155,
+    level: 5,
+  },
+
+  TOTODILE: {
+    speciesId: 158,
+    level: 5,
+  },
+
+  TREECKO: {
+    speciesId: 252,
+    level: 5,
+  },
+
+  TORCHIC: {
+    speciesId: 255,
+    level: 5,
+  },
+
+  MUDKIP: {
+    speciesId: 258,
+    level: 5,
+  },
+
+  TURTWIG: {
+    speciesId: 387,
+    level: 5,
+  },
+
+  CHIMCHAR: {
+    speciesId: 390,
+    level: 5,
+  },
+
+  PIPLUP: {
+    speciesId: 393,
+    level: 5,
+  },
+} as const;
